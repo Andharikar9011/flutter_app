f04849b8ddce29f726e4add4371c613f8712cdec
Andharikar9011 <48117912+Andharikar9011@users.noreply.github.com>
2020-06-03 21:28:46 +0530

    change 1

--- lib/main.dart
+++ lib/main.dart
@@ -11,49 +11,24 @@ var app_secret =  "e28083ec-7390-4a29-a803-2dd159910ed0";
 await AppCenter.start(app_secret, [AppCenterAnalytics.id, AppCenterCrashes.id]);
 runApp(MyApp());
 }
-class MyApp extends StatefulWidget {
-@override
 
-State<StatefulWidget> createState(){
-	return _MyAppState();
-	}
-}
-class _MyAppState extends State<MyApp> {
-	List<String> _products = ['Food Tester'];
-
-@override
+class MyApp extends StateLessWidget{
 Widget build(BuildContext context){
 return MaterialApp(
 	home: Scaffold(
 		appBar: AppBar(
 			title:Text('Second APP'),
 			),
-		body: 
-			Column( 
-				children:[
-				Container(
-					margin: EdgeInsets.all(10.0),
-					child: RaisedButton(
-						onPressed:() {
-							setState((){
-							_products.add('Advances Food Tester');
-							print(_products);
-								});
-							},
-						child :Text('Add Product'),
-
-						),
-					),
-
-
-				 Column( 
-				 children:  
-					_products.map((element) => Card(child: Column(children:<Widget>[Image.asset('assets/food.jpg'),Text(element)] ))).toList(),
-						
-					),
-						
+		body: Card(
+			child: Column(
+				children:<Widget>[
+				Image.asset(assets/food.jpg),
+				Text('Food'),
+				Image.asset(assets/food.jpg),
+				Text('Food')
 				]
-			)
+				),
+			),
 		),
 	);
 }
